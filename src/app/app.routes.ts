f8901b7a1782579ffc8be2df1609533361b47bf1
Jelly <jellyquenmaniscan@gmal.com>
2026-02-14 23:16:31 +0800

    a

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { Login } from './components/auth/login/login';
+import { Home } from './components/home/home';
 
 
 export const routes: Routes = [
   { path: 'login', component: Login },
+  { path: 'home', component: Home },
+
 
   // default route
   { path: '', redirectTo: 'login', pathMatch: 'full' }
--- src/app/components/auth/login/login.ts
+++ src/app/components/auth/login/login.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import { RouterLink } from "@angular/router";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,11 +13,14 @@ export class Login {
   email = '';
   password = '';
   message = '';
-role: any;
+role = 'admin';
+ constructor(private router: Router) {}
 
 login() {
+  
     if (this.email === 'maniscan@brims.com' && this.password === '1234' ) {
       this.message = 'Login successful!';
+        this.router.navigate(['/home']);
     } else {
       this.message = 'Invalid credentials!';
     }
