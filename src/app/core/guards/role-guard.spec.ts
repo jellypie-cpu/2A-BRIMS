28c2f7ff59ec66869a7b8b6a67f562b7a3995279
Jelly <jellyquenmaniscan@gmal.com>
2026-04-25 15:13:02 +0800

    guard

--- src/app/core/guards/role-guard.spec.ts
+++ src/app/core/guards/role-guard.spec.ts
@@ -1,17 +1,17 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import { RoleGuard } from './role-guard';
 
-import { roleGuard } from './role-guard';
+describe('RoleGuard', () => {
 
-describe('roleGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => roleGuard(...guardParameters));
+  let guard: RoleGuard;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
+    guard = TestBed.inject(RoleGuard);
   });
 
   it('should be created', () => {
-    expect(executeGuard).toBeTruthy();
+    expect(guard).toBeTruthy();
   });
+
 });
